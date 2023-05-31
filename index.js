26fa12f67bc785cf88e1bba5a275e5b4703fe598
Montell Francis <montelldfrancis@gmail.com>
2023-05-31 16:12:19 -0400

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+destructivelyAppendCat(Ralph);
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const newArray = [...cats, name];
+    return newArray;
+}
+function prependCat(name) {
+    const newArray = [name, ...cats];
+    return newArray;
+}
+function prependCat(name) {
+    const newArray = [name, ...cats];
+    return newArray;
+}
+function removeLastCat() {
+    const newArray = cats.slice(0, 2);
+    return newArray;
+}
+
+function removeFirstCat() {
+    const newArray = cats.slice(1);
+    return newArray;
+}
